2298a688d317374b7280b88a580b28f0424dfeb4
ruzel <you@example.com>
2019-10-07 14:12:26 +0500

    added tets index

---
+++ config/routes.js
@@ -0,0 +1,40 @@
+//--Подтягиваем контроллеры на создание удалиени изменения... продукта
+const products = require('../app/controllers/products');
+//--Подтягиваем контроллер авторизации
+const auth = require('../app/controllers/auth');
+
+//--Подтягиваем модуль промежуточного слоя для авторизауии
+const authMiddleware = require('../app/middleware/auth');
+
+//--Экпорт роутов Api
+module.exports = (app) => {
+
+    app.get('/', (req, res) => {
+
+        res.send("index");
+
+    });
+
+    /*
+        Поток прохождения запроса:
+        ://запрос / проверка промежуточного слоя на авторизацию / выполнение запроса
+
+        если authMiddleware не прошел то отправляет не соответствие jwt-токена
+    */
+
+    //products
+    app.get('/products', authMiddleware, products.getAll);
+    app.post('/products', authMiddleware, products.create);
+    app.put('/products/:id', authMiddleware, products.update);
+    app.delete('/products/:id', authMiddleware, products.remove);
+
+
+    /*
+        Поток прохождения запроса на авторизацию:
+        ://запрос / выполнение запроса
+    */
+
+    //auth
+    app.post('/signin', auth.signIn);
+    app.post('/refresh-tokens', auth.refreshTokens);
+};
